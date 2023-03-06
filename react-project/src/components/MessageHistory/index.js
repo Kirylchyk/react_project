b0a7d1b63fc349d65d599b788c6a3a9227d61c8f
varvara <kirylchyk@gmail.com>
2023-03-05 20:28:42 -0500

    change03

---
+++ react-project/src/components/MessageHistory/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import styles from './MessageHistory.module.css';
+
+function MessageHistory ({messages}) {
+
+    return (
+        <ul>
+            {messages.map(message => {
+                return (
+                    <li className={styles.chat} key={message.id}>
+                        <div>
+                            Author: {message.from.name} {message.time}
+                        </div>
+                        <div>
+                            Text: {message.text} {message.type}
+                        </div>
+                    </li>
+                )
+            })}
+        </ul>
+    );
+}
+
+export default MessageHistory
+
+
--- react-project/src/components/ShopCard/index.js
+++ react-project/src/components/ShopCard/index.js
@@ -4,13 +4,16 @@ function ShopCard({item}) {
 
     return (
         <ul>
-            {item.map(item => <li key={item.id}>{item.brand}</li>)}
-            {item.map(item => <li key={item.id}>{item.title}</li>)}
-            {item.map(item => <li key={item.id}>{item.description}</li>)}
-            {item.map(item => <li key={item.id}>{item.descriptionFull}</li>)}
-            {item.map(item => <li key={item.id}>{item.price}</li>)}
-            {item.map(item => <li key={item.id}>{item.currency}</li>)}
+            {/*{item.map(item => <li key={item.id}>{item.brand}</li>)}*/}
+            {/*{item.map(item => <li key={item.id}>{item.title}</li>)}*/}
+            {/*{item.map(item => <li key={item.id}>{item.description}</li>)}*/}
+            {/*{item.map(item => <li key={item.id}>{item.descriptionFull}</li>)}*/}
+            {/*{item.map(item => <li key={item.id}>{item.price}</li>)}*/}
+            {/*{item.map(item => <li key={item.id}>{item.currency}</li>)}*/}
+            {item.map(item => <li key={item.id}>{Object.entries(item)}</li>)}
         </ul>
+
+
 
     );
 
